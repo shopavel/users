382db502cc3b751a39b0c62e78ceb93b6382bf02
Laurence Roberts <lsjroberts@gmail.com>
2013-09-04 01:16:04 +0100

    Initial commit

---
+++ src/Shopavel/Users/User.php
@@ -0,0 +1,9 @@
+<?php namespace Shopavel\Users;
+
+use Cartalyst\Sentry\Users\Eloquent\User as SentryUser;
+
+class User extends SentryUser {
+
+    
+
+}
---
+++ src/Shopavel/Users/UserInterface.php
@@ -0,0 +1,8 @@
+<?php namespace Shopavel\Users;
+
+interface UserInterface {
+
+    public function getUser();
+    public function setUser(User $user);
+
+}
